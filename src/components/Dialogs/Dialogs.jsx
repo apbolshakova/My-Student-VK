e4953366c2dc4cf1b60e633d1265faf4dacc6472
Анжела Большакова <julody@mail.ru>
2020-09-22 13:02:06 +0300

    Захардкоженные данные вынесены из тегов в массивы

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -15,21 +15,37 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Екатерина Анисимова'},
+        {id: 2, name: 'Егор Корнев'},
+        {id: 3, name: 'Елизавета Логинова'},
+        {id: 4, name: 'Ксения Тихомирова'},
+        {id: 5, name: 'Марк Фомин'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Привет!'},
+        {id: 2, message: 'Пришли домашку по вышмату, пожалуйста'},
+        {id: 3, message: 'Ау'},
+        {id: 4, message: 'Ты игноришь?'}
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Екатерина Анисимова" id="1" />
-                <DialogItem name="Илья Васильев" id="2" />
-                <DialogItem name="Егор Корнев" id="3" />
-                <DialogItem name="Елизавета Логинова" id="4" />
-                <DialogItem name="Ксения Тихомирова" id="5" />
-                <DialogItem name="Марк Фомин" id="6" />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name="" id="3" />
+                <DialogItem name="" id="4" />
+                <DialogItem name="" id="5" />
+                <DialogItem name="" id="6" />
             </div>
             <div className={s.messages}>
-                <Message message="Привет!" />
-                <Message message="Пришли домашку по вышмату, пожалуйста" />
-                <Message message="Ау" />
-                <Message message="Ты игноришь?" />
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+                <Message message={messagesData[2].message} />
+                <Message message={messagesData[3].message} />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -3,6 +3,12 @@ import s from './MyPost.module.css';
 import Post from './Post/Post';
 
 const MyPost = (props) => {
+
+    let postData = [
+        {id: 1, message: 'Хорошо выглядишь!', likesCount: 2},
+        {id: 2, message: 'Учи реакт', likesCount: 25},
+    ];
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPost = (props) => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message='Хорошо выглядишь!' likesCount='0'/>
-                <Post message="Учи реакт" likesCount='23'/>
+                <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+                <Post message={postData[1].message} likesCount={postData[1].likesCount} />
             </div>
         </div>
     )
