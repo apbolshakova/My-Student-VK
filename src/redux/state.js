ecab2c83708aab4fb087667c319fb91a8677a4ec
Анжела Большакова <julody@mail.ru>
2020-10-09 19:16:30 +0300

    Бизнес-логика вынесена в один класс store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,79 +1,84 @@
-let rerenderEntireTree = () => {
-    console.log('State changed');
-}
+let store = {
+    _state: {
+        navbar: {
+            bestFriends: [
+                {id: 2, name: 'Екатерина Анисимова'},
+                {id: 3, name: 'Егор Корнев'},
+                {id: 4, name: 'Елизавета Логинова'}
+            ]
+        },
+        profilePage: {
+            posts: [
+                {id: 4, message: 'Хорошо выглядишь!', likesCount: 2},
+                {id: 3, message: 'Учи реакт', likesCount: 25},
+                {id: 4, message: 'Привет', likesCount: 0},
+                {id: 5, message: 'Пока', likesCount: 24}
+            ],
+            newPostText: 'Введите текст поста...'
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Привет!'},
+                {id: 2, message: 'Пришли домашку по вышмату, пожалуйста'},
+                {id: 3, message: 'Ау'},
+                {id: 4, message: 'Ты игноришь?'}
+            ],
+            dialogs: [
+                {id: 1, name: 'Екатерина Анисимова'},
+                {id: 2, name: 'Егор Корнев'},
+                {id: 3, name: 'Елизавета Логинова'},
+                {id: 4, name: 'Ксения Тихомирова'},
+                {id: 5, name: 'Марк Фомин'},
+                {id: 6, name: 'Дмитрий Побрацкий'}
+            ],
+            newMessageText: 'Введите сообщение...'
+        }
+    },
 
-let state = {
-    navbar: {
-        bestFriends: [
-            {id: 2, name: 'Екатерина Анисимова'},
-            {id: 3, name: 'Егор Корнев'},
-            {id: 4, name: 'Елизавета Логинова'}
-        ]
+    getState() {
+        return this._state;
     },
-    profilePage: {
-        posts: [
-            {id: 4, message: 'Хорошо выглядишь!', likesCount: 2},
-            {id: 3, message: 'Учи реакт', likesCount: 25},
-            {id: 4, message: 'Привет', likesCount: 0},
-            {id: 5, message: 'Пока', likesCount: 24}
-        ],
-        newPostText: 'Введите текст поста...'
+
+    _callSubscriber() {
+        console.log('State changed');
     },
-    dialogsPage: {
-        messages: [
-            {id: 1, message: 'Привет!'},
-            {id: 2, message: 'Пришли домашку по вышмату, пожалуйста'},
-            {id: 3, message: 'Ау'},
-            {id: 4, message: 'Ты игноришь?'}
-        ],
-        dialogs: [
-            {id: 1, name: 'Екатерина Анисимова'},
-            {id: 2, name: 'Егор Корнев'},
-            {id: 3, name: 'Елизавета Логинова'},
-            {id: 4, name: 'Ксения Тихомирова'},
-            {id: 5, name: 'Марк Фомин'},
-            {id: 6, name: 'Дмитрий Побрацкий'}
-        ],
-        newMessageText: 'Введите сообщение...'
+
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    addMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newMessageText
+        };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-}
+};
 
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 5,
-        message: state.dialogsPage.newMessageText
-    };
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+window.store = store;
+export default store;
